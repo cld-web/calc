41759ad0756e61062712eff6ca7f02f2e4125de8
Cl <toad_tales@outlook.com>
2026-03-16 13:27:51 -0300

    mod15

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#ec4899", // rosa principal
+        },
+      },
+    },
+  },
+  plugins: [],
+}
